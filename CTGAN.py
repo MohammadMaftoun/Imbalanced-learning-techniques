7fd43c90249aa4b3f2b907716d00a39a2d0127e1
Mohammad Maftoun <101670540+MohammadMaftoun@users.noreply.github.com>
2024-11-18 12:21:00 +0330

    Update CTGAN.py

--- CTGAN.py
+++ CTGAN.py
@@ -1,1 +1,19 @@
 #CTGAN Libraries
+from ctgan import CTGAN
+
+
+cat_feature = ['example1','example2']
+
+ctgan = CTGAN(verbose=True)
+ctgan.fit(data, cat_feature, epochs = 200)
+
+# Saving ctgan
+ctgan.save('file.pkl')
+
+# Creating samples
+samples = ctgan.sample()
+
+# Fixing class imbalance problem
+samples = samples[samples['target'] ==1]
+ctgan_result_df = pd.concat([data,samples])
+ctgan_result_df.head()
